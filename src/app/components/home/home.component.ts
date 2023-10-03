97748685733782e93aee8a6cc738c6fea458f562
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-03 14:16:10 +0530

    ng

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,11 +10,20 @@ import { Component } from '@angular/core';
 
 export class HomeComponent {
 
+
     firstName: string = 'Sonu'; // from database 
     lastName: string = 'Singh';
     empData = { eid: 101, firstName: 'Monu' };
-
     userData: string = '';
+
+    salary: number = 100;
+
+    increaseSalary = () => {
+        this.salary += 1;
+    };
+
+    imageSource: string = 'https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FtcGxlfGVufDB8fDB8fHww&w=1000&q=80';
+
 
 }
 
@@ -29,9 +38,8 @@ export class HomeComponent {
 //   display here    <--   send from here
 
 // interpolation - component to template
+// property binding - component to template
 
 // event binding - template to component
-
-// property binding - component to template
 
 // two-way binding - two way  
