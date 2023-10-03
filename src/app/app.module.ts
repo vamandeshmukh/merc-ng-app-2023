c96bc55576759624ddd02f825060eb1c41f6d5ab
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-03 11:29:33 +0530

    ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-home',
-    // templateUrl: './home.component.html'
-    template: `<p>This is home component. abc</p>`
+    templateUrl: './home.component.html',
+    // template: `<p>This is home component. abc</p>`
+    // styles: [`p {color: 'blue'}`]
 })
 export class HomeComponent {
 
