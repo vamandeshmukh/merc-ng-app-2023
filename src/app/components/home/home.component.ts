61485b0b4c282f5bd1b4ce50faad8e3265249d09
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-03 11:57:21 +0530

    ng

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 
+
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     // template: `<p>This is home component. abc</p>`
     // styles: [`p {color: 'blue'}`]
 })
+
 export class HomeComponent {
+
+    firstName: string = 'Sonu'; 
+    lastName: string = 'Singh'; 
 
 }
 
+
+
+
+
+// data binding
+
+// template (HTML)     component (Script)
+
+//   display here    <--   send from here
 
 
