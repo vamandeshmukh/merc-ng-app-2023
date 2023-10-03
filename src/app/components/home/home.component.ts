4c9b068d406f29911b82f8c8b6dfecfd2d7e0775
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-03 11:00:10 +0530

    ng

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-home',
-    templateUrl: './home.component.html'
+    // templateUrl: './home.component.html'
+    template: `<p>This is home component. abc</p>`
 })
 export class HomeComponent {
 
