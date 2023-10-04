c03a208418c8a9cd36ab5c282631ef7a31e76bd4
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-04 17:17:06 +0530

    ng

--- src/app/components/blog-details/blog-details.component.ts
+++ src/app/components/blog-details/blog-details.component.ts
@@ -1,14 +1,7 @@
-
-// add comments to this blog post 
-// comments component 
-// comment model interface 
-// comment service 
-
 import { Component } from '@angular/core';
 import { BlogDataService } from 'src/app/services/blog-data.service';
 import { Blog } from 'src/app/models/blog.model';
-import { ActivatedRoute } from '@angular/router';
-
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-blog-details',
@@ -19,20 +12,71 @@ export class BlogDetailsComponent {
 
   blogId: number | any = '';
   blogData: Blog | any = '';
+  blogNotAvailable: any = '';
 
   constructor(private actRoute: ActivatedRoute, private blogDataService: BlogDataService) { }
 
   ngOnInit() {
     this.blogId = this.actRoute.snapshot.paramMap.get('id');
-
+    console.log(this.blogId);
     this.blogDataService.getBlogById(this.blogId)
       .subscribe(
-        (resp) => {
-          console.log(resp);
-          this.blogData = resp;
-        });
+        {
+          next: (resp) => {
+            this.blogData = resp;
+            console.log(resp);
+          },
+          error: (err) => {
+            console.error(err);
+            if (err.status === 404)
+              this.blogNotAvailable = `Sorry! Blog with the id ${this.blogId} is not found.`;
+          },
+          complete: () => {
+            console.info('complete');
+          }
+        }
+      );
+
+
+
   }
+
 }
+
+
+// add comments to this blog post 
+// comments component 
+// comment model interface 
+// comment service 
+
+// import { Component } from '@angular/core';
+// import { BlogDataService } from 'src/app/services/blog-data.service';
+// import { Blog } from 'src/app/models/blog.model';
+// import { ActivatedRoute, ParamMap } from '@angular/router';
+
+// @Component({
+//   selector: 'app-blog-details',
+//   templateUrl: './blog-details.component.html',
+//   styleUrls: ['./blog-details.component.css']
+// })
+// export class BlogDetailsComponent {
+
+//   blogId: number | any = '';
+//   blogData: Blog | any = '';
+
+//   constructor(private actRoute: ActivatedRoute, private blogDataService: BlogDataService) { }
+
+//   ngOnInit() {
+//     this.blogId = this.actRoute.snapshot.paramMap.get('id');
+//     this.blogDataService.getBlogById(this.blogId)
+//       .subscribe(
+//         (resp) => {
+//           console.log(resp);
+//           this.blogData = resp;
+//         });
+//   }
+// }
+
 
 
 // import { Component } from '@angular/core';
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,26 +3,37 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    // template: `<p>This is home component. abc</p>`
-    // styles: [`p {color: 'blue'}`]
 })
 export class HomeComponent {
 
-    firstName: string = 'Sonu'; // from database 
-    lastName: string = 'Singh';
-    empData = { eid: 101, firstName: 'Monu' };
-    userData: string = '';
-
-    salary: number = 100;
-
-    increaseSalary = () => {
-        this.salary += 1;
-    };
-
-    imageSource: string = 'https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FtcGxlfGVufDB8fDB8fHww&w=1000&q=80';
-
 
 }
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//     selector: 'app-home',
+//     templateUrl: './home.component.html',
+//     // template: `<p>This is home component. abc</p>`
+//     // styles: [`p {color: 'blue'}`]
+// })
+// export class HomeComponent {
+
+//     firstName: string = 'Sonu'; // from database
+//     lastName: string = 'Singh';
+//     empData = { eid: 101, firstName: 'Monu' };
+//     userData: string = '';
+
+//     salary: number = 100;
+
+//     increaseSalary = () => {
+//         this.salary += 1;
+//     };
+
+//     imageSource: string = 'https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FtcGxlfGVufDB8fDB8fHww&w=1000&q=80';
+
+
+// }
 
 
 // data binding
