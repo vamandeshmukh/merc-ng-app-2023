1caf829da408ba4e3ef0422d8e9834aea44cad3c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-04 13:07:37 +0530

    ng

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogDetailsComponent } from './components/blog-details/blog-details.component';
+import { BlogListComponent } from './components/blog-list/blog-list.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {},
-  {},
-  {}
+  { path: 'home', component: HomeComponent },
+  { path: 'all-blogs', component: BlogListComponent },
+  // { path: 'blog-details', component: BlogDetailsComponent },
+  { path: 'blog-details/:id', component: BlogDetailsComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  // more routes 
 ];
 
 @NgModule({
--- src/app/components/blog-details/blog-details.component.ts
+++ src/app/components/blog-details/blog-details.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { BlogDataService } from 'src/app/services/blog-data.service';
 import { Blog } from 'src/app/models/blog.model';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -10,20 +11,50 @@ import { Blog } from 'src/app/models/blog.model';
 })
 export class BlogDetailsComponent {
 
-  blogId: number = 22;
+  blogId: number | any = '';
   blogData: Blog | any = '';
 
-  constructor(private blogDataService: BlogDataService) { }
+  constructor(private actRoute: ActivatedRoute, private blogDataService: BlogDataService) { }
 
   ngOnInit() {
+    this.blogId = this.actRoute.snapshot.paramMap.get('id');
     this.blogDataService.getBlogById(this.blogId)
       .subscribe(
         (resp) => {
-        console.log(resp);
-        this.blogData = resp;
-      });
+          console.log(resp);
+          this.blogData = resp;
+        });
   }
 }
+
+
+// import { Component } from '@angular/core';
+// import { BlogDataService } from 'src/app/services/blog-data.service';
+// import { Blog } from 'src/app/models/blog.model';
+
+
+// @Component({
+//   selector: 'app-blog-details',
+//   templateUrl: './blog-details.component.html',
+//   styleUrls: ['./blog-details.component.css']
+// })
+// export class BlogDetailsComponent {
+
+//   blogId: number | any = '';
+//   blogData: Blog | any = '';
+
+//   constructor(private blogDataService: BlogDataService) { }
+
+//   ngOnInit() {
+//     this.blogId = 22;
+//     this.blogDataService.getBlogById(this.blogId)
+//       .subscribe(
+//         (resp) => {
+//           console.log(resp);
+//           this.blogData = resp;
+//         });
+//   }
+// }
 
 
 // import { Component } from '@angular/core';
