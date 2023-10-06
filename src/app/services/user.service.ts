bcc3658a8345480ce8158462cbc32ffe702082ad
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-06 14:04:07 +0530

    ng

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,15 +9,6 @@ export class UserService {
 
   userUrl: string = 'https://jsonplaceholder.typicode.com/users';
 
-  reference: string = 'https://data.gov.in/resource/all-india-pincode-directory-contact-details-along-latitude-and-longitude#api';
-
-  addressApi: string = 'https://api.data.gov.in/resource/5c2f62fe-5afa-4119-a499-fec9d604d5bd';
-  apiKey: string = '579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b';
-
-  getAddressByPincode = (pincode: number): Observable<any> => {
-    return this.http.get(`${this.addressApi}?api-key=${this.apiKey}&format=json&filters%5Bpincode%5D=${pincode}`);
-  };
-
   constructor(private http: HttpClient) { }
 
   register = (registerData: any): Observable<any> => {
@@ -29,6 +20,18 @@ export class UserService {
     console.log(loginData);
     return this.http.get(`${this.userUrl}?username=${loginData.username}`);
   };
+
+  // If possible, use this in register component 
+  // reference: 'https://data.gov.in/resource/all-india-pincode-directory-contact-details-along-latitude-and-longitude#api';
+
+  addressApi: string = 'https://api.data.gov.in/resource/5c2f62fe-5afa-4119-a499-fec9d604d5bd';
+  apiKey: string = '579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b';
+
+  getAddressByPincode = (pincode: number): Observable<any> => {
+    return this.http.get(`${this.addressApi}?api-key=${this.apiKey}&format=json&filters%5Bpincode%5D=${pincode}`);
+  };
+
+
 }
 
 // import { HttpClient } from '@angular/common/http';
