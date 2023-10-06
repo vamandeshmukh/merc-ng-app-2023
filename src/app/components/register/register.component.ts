c789875d40da5adcd16706312b4d5923399cefd2
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-06 17:03:09 +0530

    ng

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -17,16 +17,45 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
       username: ['', Validators.required],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
+      confirmpassword: ['', Validators.required],
+      address: this.formBuilder.group({
+        street: ['', Validators.required],
+        city: [{ value: '', disabled: true }],
+        state: [{ value: '', disabled: true }],
+        pincode: ['', Validators.required],
+      })
     });
   }
 
+  fillAddressData = () => {
+    const pin = this.registerForm.get('address.pincode').value;
+    console.log(pin);
+    this.userService.getAddressByPincode(pin)
+    // use pipe here 
+      .subscribe({
+        next: (resp) => {
+          const addressData = resp.records[0];
+          console.log(addressData.district);
+          console.log(addressData.statename);
+          this.registerForm.patchValue({
+            address: {
+              city: addressData.district,
+              state: addressData.statename
+            }
+          });
+        },
+        error: (err) => { console.log(err) }
+      })
+  };
+
   submitRegister = () => {
-    console.log('submit');
+    console.log(this.registerForm.value);
     if (this.registerForm.valid) {
       const registerData = this.registerForm.value;
       this.userService.register(registerData)
         .subscribe((resp) => {
+          console.log(resp);
           alert(`Hi ${resp.username}! You've successfully registered. Redirecting to login...`);
           this.registerForm.reset();
           this.router.navigate(['login']);
@@ -35,6 +64,7 @@ export class RegisterComponent implements OnInit {
     }
   };
 }
+
 
 
 
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, shareReplay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,17 +11,7 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  register = (registerData: any): Observable<any> => {
-    console.log(registerData);
-    return this.http.post(this.userUrl, registerData);
-  };
-
-  login = (loginData: any): Observable<any> => {
-    console.log(loginData); // side-effect 
-    return this.http.get(`${this.userUrl}?username=${loginData.username}`);
-  };
-
-  // If possible, use this in register component 
+  // Use this in register component 
   // reference: 'https://data.gov.in/resource/all-india-pincode-directory-contact-details-along-latitude-and-longitude#api';
 
   addressApi: string = 'https://api.data.gov.in/resource/5c2f62fe-5afa-4119-a499-fec9d604d5bd';
@@ -31,8 +21,74 @@ export class UserService {
     return this.http.get(`${this.addressApi}?api-key=${this.apiKey}&format=json&filters%5Bpincode%5D=${pincode}`);
   };
 
+  // username:
+	// password:
+	// confirm-password:
+	// address:
+	// 	street:
+	// 	city: 
+	// 	state:
+	// 	pin: 500001
+	// 	country: India 
+
+
+
+  register = (registerData: any): Observable<any> => {
+    console.log(registerData);
+    return this.http.post(this.userUrl, registerData)
+      .pipe(shareReplay(1, 20000))
+  };
+
+  // login = (loginData: any): Observable<any> => {
+  //   console.log(loginData);  
+  //   return this.http.get(`${this.userUrl}?username=${loginData.username}`);
+  // };
+
+  login = (loginData: any): Observable<any> => {
+    console.log(loginData);
+    return this.http.get(`${this.userUrl}?username=${loginData.username}`);
+  };
+
+
 
 }
+
+
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class UserService {
+
+//   userUrl: string = 'https://jsonplaceholder.typicode.com/users';
+
+//   constructor(private http: HttpClient) { }
+
+//   register = (registerData: any): Observable<any> => {
+//     console.log(registerData);
+//     return this.http.post(this.userUrl, registerData);
+//   };
+
+//   login = (loginData: any): Observable<any> => {
+//     console.log(loginData); // side-effect 
+//     return this.http.get(`${this.userUrl}?username=${loginData.username}`);
+//   };
+
+//   // If possible, use this in register component 
+//   // reference: 'https://data.gov.in/resource/all-india-pincode-directory-contact-details-along-latitude-and-longitude#api';
+
+//   addressApi: string = 'https://api.data.gov.in/resource/5c2f62fe-5afa-4119-a499-fec9d604d5bd';
+//   apiKey: string = '579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b';
+
+//   getAddressByPincode = (pincode: number): Observable<any> => {
+//     return this.http.get(`${this.addressApi}?api-key=${this.apiKey}&format=json&filters%5Bpincode%5D=${pincode}`);
+//   };
+
+
+// }
 
 // import { HttpClient } from '@angular/common/http';
 // import { Injectable } from '@angular/core';
